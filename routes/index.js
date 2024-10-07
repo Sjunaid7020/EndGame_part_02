a69fa39d270a9a653b6dc112cf0db7093623a4b4
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-10-07 20:59:47 +0530

    mongodb first Q done

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const userModel = require('./users')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/failed', function(req, res) {
-  req.flash("age", 25)
-  req.flash("name", 'ABC ')
-  res.send("its done")
-});
+router.get('/create', async function(req, res){
+ let userdata = await userModel.create({
+     username:'Jon',
+     nickname:'J',
+     description:'Developer',
+     categories:['js','React','node'],
 
-router.get('/check', function(req, res) {
-  console.log(req.flash("age" ))
-  console.log(req.flash("name" ))
-  res.send('check console')
+ })
+    res.send(userdata)
 });
-
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require("mongoose")
+const {data} = require("express-session/session/cookie");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+mongoose.connect("mongodb://127.0.0.1:27017/EndGame_02_Db")
 
-module.exports = router;
+const userSchema = mongoose.Schema({
+  username:String,
+  nickname:String,
+  description:String,
+  categories:{
+    type:Array,
+    default:[]
+  },
+  datecreated:{
+    type:Date,
+    default: Date.now()
+  }
+})
+
+ module.exports = mongoose.model('user',userSchema)
