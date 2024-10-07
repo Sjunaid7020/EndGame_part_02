c882ffc82067424046e33044cabd7e4aac1394ec
Sjunaid7020 <shaikhjunaid7020@gmail.com>
2024-10-07 20:36:07 +0530

    connect-flash implanted

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/failed', function(req, res) {
+  req.flash("age", 25)
+  req.flash("name", 'ABC ')
+  res.send("its done")
+});
+
+router.get('/check', function(req, res) {
+  console.log(req.flash("age" ))
+  console.log(req.flash("name" ))
+  res.send('check console')
+});
+
+
 module.exports = router;
